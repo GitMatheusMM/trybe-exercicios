30460522682c01652160dca8026301ea06d97f0b
GitMatheusMM <matheus_matos_8@hotmail.com>
2023-10-14 19:18:00 -0300

    SEcond exercise

--- fundamentos/secao-5-HTML-e-CSS-FORMS-flexbox-e-responsivo/dia-1-html-e-css-forms/script.js
+++ fundamentos/secao-5-HTML-e-CSS-FORMS-flexbox-e-responsivo/dia-1-html-e-css-forms/script.js
@@ -3,8 +3,8 @@ const inputText = document.querySelector("#input-text");
 const inputCheckbox = document.querySelector("#input-checkbox");
 const hrefLink = document.querySelector("#href");
 
-window.onload = () => {
- hrefLink.addEventListener('click', (event) => {
-  event.preventDefault();
- });
-}
+// window.onload = () => {
+//  hrefLink.addEventListener('click', (event) => {
+//   event.preventDefault();
+//  });
+// }
